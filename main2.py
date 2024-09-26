1211fe8ca09cf9aa46658a114c314d132c3c8cef
nickk028 <ifalcone@alumno.huergo.edu.ar>
2024-09-26 15:34:48 -0300

    wds
    :)

--- main2.py
+++ main2.py
@@ -1,11 +1,10 @@
 import tkinter
-
+from ManejoDeDatos import *
 import pygame
 
 
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
-fuente = pygame.font.Font("fuentes/Averia_Libre/AveriaLibre-Regular.ttf", 31)
 running = True
 
 
@@ -46,46 +45,31 @@ fondos = [ fondo_iniciar_sesion, fondo_registrarse,
            fondo_mantenimiento_notificaciones_archivadas, fondo_menu_recepcionista, fondo_recepcionista_notificar,
            fondo_recepcinista_eventos, fondo_recepcinista_limpieza]
 
-texto_seleccionado = str()
+fuente = pygame.font.Font("fuentes/Averia_Libre/AveriaLibre-Regular.ttf", 31)
+
+texto1 = [(0,0), ""]
+texto2 = [(0,0), ""]
+texto3 = [(0,0), ""]
+texto4 = [(0,0), ""]
+texto5 = [(0,0), ""]
+texto6 = [(0,0), ""]
+texto7 = [(0,0), ""]
+texto8 = [(0,0), ""]
+
+
+texto_seleccionado = ["", texto1 ]
+texto_ingresado = ""
 
 
 
 
-def mostrar_fondo(fondo):
-    if fondo_actual[0] == fondo:
-        screen.blit (fondos[fondo_actual[1]] , (0 , 0))
 
 
 
 
-def escribir(event):
-    if event.type == pygame.KEYDOWN:
-            
-        if event.key == pygame.K_RETURN:
-            # Al presionar Enter, guardar el texto ingresado y limpiar el campo
-            texto_entrado = texto_ingresado
-            enter = True
-            texto_ingresado = ""
-            campo_activo = False
-            
-        elif event.key == pygame.K_BACKSPACE:
-            # Al presionar Retroceso, eliminar el último carácter del texto
-            texto_ingresado = texto_ingresado[:-1]
-            
 
-        elif event.key == pygame.K_ESCAPE:
-            creando = False
-            agregando = False
-            modificando = False
-            campo_activo = False
-            enter = False
-            una_vez = 0
-            contador = 0
 
-            
-        else:                                    
-        # Agregar caracteres al texto ingresado
-            texto_ingresado += event.unicode    
+
 
 
 
@@ -104,7 +88,10 @@ def cursor(mouse_pos):
         pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
 
 
-mostrar_fondo("iniciar sesion")
+
+
+
+
 while running:
 
 
@@ -120,24 +107,55 @@ while running:
 
 
 
-
+        if event.type == pygame.KEYDOWN:
+                     
+            if event.key == pygame.K_BACKSPACE:
+                # Al presionar Retroceso, eliminar el último carácter del texto
+                texto_ingresado = texto_ingresado[:-1]
+                
+    
+            elif event.key == pygame.K_ESCAPE:
+                texto_seleccionado[0] = ""
+                
+            else:                                    
+            # Agregar caracteres al texto ingresado
+                texto_ingresado += event.unicode
+            texto_seleccionado[1][1] = texto_ingresado
+            
+                
 
 
 
 
         if event.type == pygame.MOUSEBUTTONDOWN :
             print(mouse_pos)
-            if mouse_pos[0] <= 845 and mouse_pos[0] >= 439 and mouse_pos[1] <= 525 and mouse_pos[1] >= 484:
-                texto_seleccionado = "Usuario / iniciar sesion"
-            
-            elif mouse_pos[0] <= 845 and mouse_pos[0] >= 439 and mouse_pos[1] <= 631 and mouse_pos[1] >= 587:
-                texto_seleccionado = "Contraseña / iniciar sesion"
+            if fondo_actual[0] == "iniciar sesion":
+               
+                if mouse_pos[0] <= 845 and mouse_pos[0] >= 439 and mouse_pos[1] <= 525 and mouse_pos[1] >= 484:
+                    texto_ingresado = texto1[1]
+                    texto_seleccionado[0] = "Usuario / iniciar sesion"
+                    texto_seleccionado[1] = texto1
+ 
+                   
+
                 
+                elif mouse_pos[0] <= 845 and mouse_pos[0] >= 439 and mouse_pos[1] <= 631 and mouse_pos[1] >= 587:
+                    texto_ingresado = texto2[1]
+                    texto_seleccionado[0] = "Contraseña / iniciar sesion"
+                    texto_seleccionado[1] = texto2
+
+
             
         
         cursor(mouse_pos)
 
-
+    #fondos :)
+    if fondo_actual[0] == "iniciar sesion":
+        texto1[0] = ( 439, 484 )
+        texto2[0] = ( 439, 587 )
+        screen.blit (fondos[fondo_actual[1]] , (0 , 0))       
+        screen.blit( fuente.render(texto1[1] , 0 , (0,0,0) ), texto1[0] )
+        screen.blit( fuente.render(texto2[1] , 0 , (0,0,0) ), texto2[0] )
 
 
 

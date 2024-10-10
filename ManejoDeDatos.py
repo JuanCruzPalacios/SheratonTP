a516f324980cacebd98fde66732eeba2e39da212
EzeAbelle <eabelle@alumno.huergo.edu.ar>
2024-10-10 13:43:47 -0300

    ver fecha final

--- ManejoDeDatos.py
+++ ManejoDeDatos.py
@@ -87,6 +87,14 @@ def ExisteUsuario(Usuario):
         print("El usuario no existe.")
         return False
 
+def VerFechaFinal(cliente):
+    contenido = LeerJson("Usuario.json")
+    contenido2 = LeerJson("habitaciones.json")
+    habitaciones = ["suite_balcon","habitacion_triple","habitacion_doble","habitacion_lujo","habitacion_cuadruple","suite_rio","habitacion_individual","suite_jacuzzi","suite_estandar"]
+    for x in range(0,8):
+        if contenido[cliente]["IdHabitacion"] == contenido2[(habitaciones[x])]["IdClienteOcupante"]:
+            return contenido2[(habitaciones[x])]["FechaFinal"]
+
 def TieneHabitacion(usuario): 
     try:
         usuarios_json = LeerJson("Usuario.json")
@@ -142,6 +150,10 @@ def ActualizarJson(archivo, datos):
         print(f"Archivo {archivo} actualizado correctamente.")
     except Exception as e:
         print(f"Ocurrió un error al actualizar el archivo: {e}")
+
+def VerNumeroEstacionamiento(cliente):
+    contenido = LeerJson("Usuario.json")
+    return contenido[cliente]["IdEstacionamiento"]
 
 def VerNotificacion(numero_de_notificacion):
     numero_de_notificacion = str(numero_de_notificacion) 
@@ -268,3 +280,4 @@ def CrearActualizarUsuario(Usuario, Correo , Tipo , Nombre , Apellido , Contrase
 
 
     ActualizarJson("Usuario.json", contenido_actual)
+

2f1cb6ad15edb2784526772745079adf21a4b25d
IgnPine <ipineda@alumno.huergo.edu.ar>
2024-10-16 09:34:06 -0300

    actuañ

--- main2.py
+++ main2.py
@@ -866,8 +866,8 @@ while running:
                         alter_usuario = not(alter_usuario)
                         print(alter_usuario)
 
-
-
+                if fondo_actual[0] in [ "reservar eventos","ver datos","habitacion balcon", "habitacion triple", "habitacion doble", "habitacion lujo", "habitacion cuadruple", "suite rio", "habitacion individual", "suite jacuzzi", "suite estandar"] and alter_mouse == False:        
+                    alter_mouse = True
                     if mouse_pos[0] < 1008 and mouse_pos[0] > 785 and mouse_pos[1] < 666 and mouse_pos[1] > 605:#reservar
                         fondo_actual[0] = "reservar habitacion"
                         fondo_actual[1] = 5
@@ -949,7 +949,10 @@ while running:
         elif fondo_actual[0] == "menu parking":
             screen.blit (fondos[fondo_actual[1]] , (0 , 0))
             texto1[0] = (390, 346)
-            texto1[1] = str(VerNumeroHabitacion(usuario).pop(-1))
+            try:
+                texto1[1] = str(VerNumeroHabitacion(usuario).pop(-1))
+            except:
+                """"""
             screen.blit( fuente.render(texto1[1] , True , (0,0,0) ), texto1[0] )
 
 

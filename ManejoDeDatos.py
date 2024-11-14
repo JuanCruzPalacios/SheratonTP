3e2c79e16ba538d75cfa9da5ba9cb81fddf6e774
EzeAbelle <eabelle@alumno.huergo.edu.ar>
2024-11-14 15:33:08 -0300

    Update ManejoDeDatos.py

--- ManejoDeDatos.py
+++ ManejoDeDatos.py
@@ -578,7 +578,7 @@ def CalcularPrecioAmenities(dia_inicio,dia_final,cantidad_amenities):
     return precio_final
 
 def CalcularPrecioEstacionamiento(dia_inicio , dia_final):
-    precio_final = 15*diferencia_dias(dia_inicio,dia_final)
+    precio_final = 30*diferencia_dias(dia_inicio,dia_final)
     print("Precio final: " , precio_final)
     return precio_final
 
